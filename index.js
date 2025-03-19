1995011394725ac641f060162d62df55165ec459
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-03-19 14:44:03 +0300

    80% done!

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// const input=document.getElementById('inputDate')
 const input=document.getElementById('inputDate')
-
+const resultSection=document.getElementById('result-section-pId')
 function calculateAge(){
+
     const birthDate=new Date(input.value)     
-      
-     
+
     const year=birthDate.getFullYear()
     const month=birthDate.getMonth()+1
     const day=birthDate.getDate()    
@@ -16,9 +17,17 @@ function calculateAge(){
     const currentDay=currentDate.getDate()
      
     let age,month2,day2;
-
     age=currentYear-year
-    console.log(age);
+    month2=currentMonth-month
+    day2=currentDay-day
+
+    if(input.value===''){
+        alert('Please')
+        return
+    }else if(month2<0){
+        age--
+        month2+=12
+    }
     
-     
+  resultSection.textContent=`You are ${age} years,${month2} months and ${day2} days old`    
 }
